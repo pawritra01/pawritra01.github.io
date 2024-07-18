f746071f9a8c4517e1b63b4bd5f7a53dd497bddd
Aritra Bhattacharjee <pawritra01@gmail.com>
2024-07-18 13:57:37 +0530

    configure vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "./docs",
+    emptyOutDir: true, // also necessary
+  },
   plugins: [react()],
-})
+});
+
